5d32df98c439b601af05e38d4d35a5fcba78cc69
Aban <aban.salah.mahmoud@gmail.com>
2025-04-24 09:55:27 +0200

    Fix typo in db name

--- database/migrations/2025_09_28_005804_create_mentorships_table.php
+++ database/migrations/2025_09_28_005804_create_mentorships_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Mentorships', function (Blueprint $table) {
+        Schema::create('mentorships', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->foreignId('mentor_id')->constrained('users')->onDelete('cascade');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Mentorships');
+        Schema::dropIfExists('mentorships');
     }
 };
