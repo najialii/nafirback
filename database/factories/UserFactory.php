75104bc718e6c35a2cff82eae9ac3b3768b56608
Aban <aban.salah.mahmoud@gmail.com>
2025-05-12 13:46:05 +0300

    Add image seeder for User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,57 +10,57 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
- protected static ?string $password;
+    protected static ?string $password;
 
- public function definition(): array
- {
-  return [
-   'name'                  => $this->faker->name(),
-   'email'                 => $this->faker->unique()->safeEmail(),
-   'profile_pic'           => $this->faker->imageUrl(300, 300, 'people', true, 'User'),
-   'phone'                 => $this->faker->phoneNumber(),
-   'title'                 => $this->faker->jobTitle(),
-   'skills'                => $this->faker->words(5),
-   'education'             => [
-    [
-     'university'  => $this->faker->company(),
-     'certificate' => $this->faker->word() . ' Degree',
-     'degree'      => $this->faker->word(),
-      'startDate' => $this->faker->year(),
-      'endDate'   => $this->faker->year(),
-     'description' => $this->faker->sentence(),
-    ],
-   ],
-   'experience'            => [
-    [
-     'title'       => $this->faker->jobTitle(),
-     'company'     => $this->faker->company(),
-     'startDate'   => $this->faker->year(),
-     'endDate'     => $this->faker->optional()->year(),
-     'description' => $this->faker->sentence(),
-    ],
-   ],
-   'location'              => [
-    'country' => $this->faker->country(),
-    'city'    => $this->faker->city(),
-   ],
-   'cv_file'               => $this->faker->url(),
-   'targeted_locations'    => $this->faker->words(3),
-   'targeted_industries'   => $this->faker->words(3),
-   'targeted_titles'       => $this->faker->words(3),
-   'career_tasks'          => $this->faker->paragraph(),
-   'completion_percentage' => $this->faker->numberBetween(0, 100),
-   'department_id'         => \App\Models\Department::inRandomOrder()->first()?->id ?? null,
-   'email_verified_at'     => now(),
-   'password'              => static::$password ??= Hash::make('password'),
-   'remember_token'        => Str::random(10),
-  ];
- }
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'profile_pic' => 'https://picsum.photos/seed/' . $this->faker->uuid . '/300/300',
+            'phone' => $this->faker->phoneNumber(),
+            'title' => $this->faker->jobTitle(),
+            'skills' => $this->faker->words(5),
+            'education' => [
+                [
+                    'university' => $this->faker->company(),
+                    'certificate' => $this->faker->word() . ' Degree',
+                    'degree' => $this->faker->word(),
+                    'startDate' => $this->faker->year(),
+                    'endDate' => $this->faker->year(),
+                    'description' => $this->faker->sentence(),
+                ],
+            ],
+            'experience' => [
+                [
+                    'title' => $this->faker->jobTitle(),
+                    'company' => $this->faker->company(),
+                    'startDate' => $this->faker->year(),
+                    'endDate' => $this->faker->optional()->year(),
+                    'description' => $this->faker->sentence(),
+                ],
+            ],
+            'location' => [
+                'country' => $this->faker->country(),
+                'city' => $this->faker->city(),
+            ],
+            'cv_file' => $this->faker->url(),
+            'targeted_locations' => $this->faker->words(3),
+            'targeted_industries' => $this->faker->words(3),
+            'targeted_titles' => $this->faker->words(3),
+            'career_tasks' => $this->faker->paragraph(),
+            'completion_percentage' => $this->faker->numberBetween(0, 100),
+            'department_id' => \App\Models\Department::inRandomOrder()->first()?->id ?? null,
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
 
- public function unverified(): static
- {
-  return $this->state(fn(array $attributes) => [
-   'email_verified_at' => null,
-  ]);
- }
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
 }
